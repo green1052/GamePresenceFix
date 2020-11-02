6a1f378a38c7aa1730c7759800245d11457315b7
green1052 <cydyellowgreen@gmail.com>
2020-11-02 09:56:07 +0900

    Add files via upload

---
+++ GamePresenceFix.plugin.js
@@ -0,0 +1,40 @@
+/**
+ * @name GamePresenceFix
+ * @version 1.0.0
+ * @description Correct errors that turn off game activity each time you run a discord.
+ * @authorId 368688044934561792
+ * @authorLink https://github.com/green1052
+ */
+
+module.exports = class GamePresenceFix {
+    getName() {
+        return "GamePresenceFix";
+    }
+
+    getDescription() {
+        return "Correct errors that turn off game activity each time you run a discord.";
+    }
+
+    getVersion() {
+        return "1.0.0";
+    }
+
+    getAuthor() {
+        return "green1052";
+    }
+
+    load() {
+        if (window.ZLibrary) {
+            ZLibrary.PluginUpdater.checkForUpdate(
+                this.getName(),
+                this.getVersion(),
+                "https://raw.githubusercontent.com/green1052/GamePresenceFix/main/GamePresenceFix.plugin.js"
+            );
+        }
+    }
+
+    start() {
+        if (!BdApi.findModuleByProps("guildPositions").showCurrentGame)
+            BdApi.findModuleByProps("updateRemoteSettings").updateLocalSettings({showCurrentGame: true});
+    }
+}
